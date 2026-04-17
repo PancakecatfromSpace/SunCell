94e70c854758ffc88840359b976b2213417a6205
Samuel H <s230067@th-ab.de>
2026-04-17 16:52:14 +0200

    removed logic referring to CircuitCalculator, added simpler one diode model without resistances for now

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
-from CircuitCalculator.Circuit.circuit import Circuit
-import CircuitCalculator.Circuit.Components.components as ccp
-from CircuitCalculator.Circuit.solution import dc_solution
 
-# Define the circuit
-circuit = Circuit([
-    ccp.dc_voltage_source(V=1, id='Vs', nodes=('1', '0')),
-    ccp.resistor(R=1, id='R', nodes=('1', '0'))
-])
+def solarIV(cell_p:int, cell_s:int, I_s:float, m:float, U_T:float, c_0:float, E:float,steps:int):
+    """
+    calculates the current voltage curve of an array of solar cells
+    
+    Args:
+    cell_p: solar cells in parralel
+    cell_s: solar cells in series
+    I_s: saturation current
+    m: diodefactor
+    U_T: Thermalvoltage
+    c_0: photo current coefficient
+    E: irradiance
+    steps: amount of values to be calculated from minimum to maximum value
+    
+    Returns:
+    U: voltages from min to max
+    I: currents from min to max
+    """
 
-# Solve the circuit
-solution = dc_solution(circuit)
-print("Test 1:")
-print(type(solution.solution), repr(solution.solution))
-print("Test 2:")
-print(dir(solution.solution)[:50])
+# implements the simple one diode model since it requires no numeric solution
+    
+    U = np.linspace(0.0, 0.6, steps)
 
-print(f'I(R)={solution.get_current("R"):2.2f}A')
-print(f'V(R)={solution.get_voltage("R"):2.2f}V')
+# vectorized implementation
+    I_ph = c_0 * E
+    diode = I_s * (np.exp(U / (m * U_T)) - 1.0)
+    I = + I_ph - diode
+
+# amount of solar cells in series and in parralel
+    U = U*cell_s
+    I = I*cell_p
+
+    return U, I
