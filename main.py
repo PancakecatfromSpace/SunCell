fd7a8a577884c280909f713130c1566f4ce3d341
Samuel H <s230067@th-ab.de>
2026-04-21 15:09:21 +0200

    added logic to controll voltage according to measured current

--- main.py
+++ main.py
@@ -53,14 +53,11 @@ def solarIV(cell_p:int, cell_s:int, I_s:float, m:float, U_T:float, c_0:float, E:
     return U, I
 
 #create two vectors and populate them with the values from a one diode model
-U_1, I_1 = solarIV(10, 300, 8.75e-3, 4.0, 25.7e-3, 3e-3, 1000, 5)
+U_1, I_1 = solarIV(5, 300, 8.75e-3, 4.0, 25.7e-3, 3e-3, 1000, 500)
 
 #populate a vector that is always in the middle between two points within the vector U_1
 U_1_mid = (U_1[:-1] + U_1[1:]) / 2.0
 I_1_mid = (I_1[:-1] + I_1[1:]) / 2.0
-
-U_2 = [50, 50]
-I_2 = [I_1.min(), I_1.max()]
 
 socket = coms.OpenSocket(SUPPLY_IP, SUPPLY_PORT, TIMEOUT_SECONDS, BUFFER_SIZE)
 
@@ -68,8 +65,8 @@ fig, ax = plt.subplots()
 #points on the plot
 ax.plot(U_1, I_1, color='C0')
 #makes points interactable and updateable
-point1, = ax.plot([], [], marker='o', color='red', markersize=8)
-point2, = ax.plot([], [], marker='o', color='green', markersize=8)
+soll, = ax.plot([], [], marker='o', color='red', markersize=8)
+ist, = ax.plot([], [], marker='o', color='green', markersize=8)
 point3, = ax.plot([], [], marker='o', color='purple', markersize=8)
 #set the limits for the plot to be the max and min values in the vector U_1 and I_1
 ax.set_xlim(U_1.min(), U_1.max())
@@ -83,12 +80,6 @@ setpoint_label = ax.text(10, 10, "Set Value")
 # adjust placement of plot to make room for slider
 fig.subplots_adjust(bottom=0.25)
 
-#draw vertical lines to mark the switching range
-for i_u in U_1_mid:
-    plt.axvline(x=i_u, color='blue')
-for i_i in I_1_mid:
-    plt.axhline(y=i_i, color='red')
-
 # Make a horizontal slider to control current
 axcurr = fig.add_axes([0.25, 0.1, 0.65, 0.03])
 
@@ -96,7 +87,7 @@ curr_slider = Slider(
     ax=axcurr,
     label='Current (amp)',
     valmin=0,
-    valmax=I_1.max(),
+    valmax=I_1.max()*1.1,
     valinit=I_1.max()/2,
 )
 
@@ -105,8 +96,6 @@ plt.show()
 
 curr = I_1[np.argmax(U_1)]
 volt = U_1.max()
-
-print(curr, volt)
 
 volt2 = 0
 power = 10
@@ -125,12 +114,45 @@ def emergency_off():
 
 #time.sleep(5)
 
+#set voltage to half the step width of the first value
+volt = U_1[1]/4
+curr = I_1[0]
+
+"""
+The position variable is important as it stores where within the range the current set point is located.
+It is to be set to zero 
+"""
+position = 0
+
 try:
     while True:
         #read the slider and update the value
         current_slider = curr_slider.val
-        #plt.annotate(f'({U0:.2f}, {I0:.2f})', xy=(U0, I0), xytext=(5, 5), textcoords='offset points', color='red')
-        
+
+        #U0 = float(coms.readVoltage(socket))
+        #I0 = float(coms.readCurrent(socket))
+
+        U0 = volt
+        I0 = curr
+
+        UM = float(coms.measureVoltage(socket))
+        IM = float(coms.measureCurrent(socket))
+
+        # logic that checks if the current is higher than the upper left border
+        if IM > I_1[0]:
+            position = 0 # mark the left most position to be the current position
+            volt = U_1[1]/4
+        # logic that checks if the current is lower than the lower right border 
+        elif IM < I_1[-1]:
+            position = U_1.size - 1 # mark the right most position to be the right most border
+            volt = U_1[-1]
+        # check if its within range and neither all the way to the right nor all the way to the left
+        if IM < I_1[position] and not position == U_1.size - 1:
+            position = position + 1
+            volt = U_1[position]
+        if IM > I_1[position - 1] and not position == 0:
+            position = position - 1
+            volt = U_1[position]
             
         # if the current or voltage is out of range, put everything to zero and end
         if coms.setCurrent(curr, MAX_CUR, MIN_CUR, socket) == -1:
@@ -143,25 +165,18 @@ try:
             emergency_off()
             raise Exception(f"Fault! Attempted to set power {power} outside range [{MIN_POWER}, {MAX_POWER}]!")
 
-        U0 = float(coms.readVoltage(socket))
-        I0 = float(coms.readCurrent(socket))
-
-        UM = float(coms.measureVoltage(socket))
-        IM = float(coms.measureCurrent(socket))
-
-        point1.set_data([U0], [I0])      # <- scalar -> sequence
-        point2.set_data([UM], [IM])
+        soll.set_data([U0], [I0])      # <- scalar -> sequence
+        ist.set_data([UM], [IM])
 
         measure_label.set_position((UM, IM+I_1.max()*0.05))
         setpoint_label.set_position((U0, I0+I_1.max()*0.05))
-        #measure_label.update
         
-        print(current_slider)
+        #print(current_slider)
 
         fig.canvas.draw_idle()
         fig.canvas.flush_events()
         
-        time.sleep(0.1)
+        time.sleep(0.01)
         i = i + 1
 except KeyboardInterrupt:
     pass
