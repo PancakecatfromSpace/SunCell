3ef0b58870cea57dcbafb97089d0167eb9bb191a
Samuel H <s230067@th-ab.de>
2026-06-01 13:30:34 +0200

    edited some values in main, added supply.measureValues to right part of programm

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from functools import partial
 supply = coms.SupplyCommunication("10.30.0.105") # connect to power supply with this IP Address
 
 #create two vectors and populate them with the values from a one diode model
-U_1, I_1 = curveutils.solarIV(5, 300, 8.75e-3, 4.0, 25.7e-3, 3e-3, 1000, 500)
+U_1, I_1 = curveutils.solarIV(5, 300, 8.75e-3, 4.0, 25.7e-3, 3e-3, 1000, 250)
 
 fig, ax = plt.subplots()
 #points on the plot
@@ -54,6 +54,7 @@ def update_supply():
     supply.setValues(curveutils.select_voltage_for_current(U_1, I_1, supply.measuredpoints.current))
 
 def update_gui():
+    supply.measureValues()
     soll.set_data([supply.setpoints.voltage], [supply.setpoints.current])      # <- scalar -> sequence
     ist.set_data([supply.measuredpoints.voltage], [supply.measuredpoints.current])
 
