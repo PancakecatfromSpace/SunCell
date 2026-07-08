ce15ce7d7f091747935ae2e16d383e993252d010
Samuel H <s230067@th-ab.de>
2026-07-08 16:44:17 +0200

    moved all logic to set the curve and determine the curve values into gui_signals, added logic to set the supply into the measure wrapper class, removed seperate scheduler entry for setting

--- gui_signals.py
+++ gui_signals.py
@@ -1,16 +1,23 @@
 """
-This file wraps the signals for the gui.py user interface into it's own file.
-In the main file of this programm the class is called into an object and then the methods of the functions are given to the scheduler.
+This file wraps the signals for the gui.py user interface into it's own file,
+contains the wrappers for the functions to be scheduled and how they connect to the UI.
 """
 
 from PySide6.QtCore import QObject, Signal, Slot
 from PySide6 import QtWidgets
-import gui
+import gui, curveutils
 import power_supply_drivers.wrapper as coms
 
 #placed this here temporarily, should be able to be removed later when the connect feature has been added to the GUI
-
 supply = coms.SupplyCommunication("10.30.0.110", lookup = "tti", port = 9221, type="VISA")
+#placed this here temporarily, should be able to be removed later when the features for editing the values has been added to the GUI
+#create two vectors and populate them with the values from a one diode model
+U_1, I_1 = curveutils.solarIV(4, 50, 8.75e-3, 4.0, 25.7e-3, 3e-3, 1000, 10000)
+#prepare data before running the controll algorithm, removes too low 
+U_1, I_1 = curveutils.min_remover(U_1, I_1, 5)
+U_1, I_1 = curveutils.stepsize_reducer(list(U_1), list(I_1), 0.025, 'right')
+set_supply = curveutils.setter(U_1, I_1)
+
 
 class MainDialog(QtWidgets.QDialog):
     """
@@ -22,6 +29,9 @@ class MainDialog(QtWidgets.QDialog):
 
         self.ui = gui.Ui_Dialog()
         self.ui.setupUi(self)
+        #eddited the value of the initial voltage to 5, by suggestion by Rüdiger Mann 27.04.26, stub left here as initial thing to run
+        #kinda out of place here, best to be removed later since it relies on I_1 being available in the namespace of the class definition
+        supply.setValues(5, I_1.max()*1.1, 3000.0)
 
         self.ui.voltage_display.value
         measure_signal.measurement_changed.connect(self.on_measurement)
@@ -32,20 +42,40 @@ class MainDialog(QtWidgets.QDialog):
 
 class psu_measure_signal(QObject):
     """
-    Wraps the call of the 
+    Wraps the call of the function to measure the supply Values into a class which contains a signal that can be processed by the UI.
+    After receiving the Data a signal is send to update the UI.
+    Also contains the logic to determine and set the output value.
     """
     measurement_changed = Signal(float, float, float)
 
-    def __init__(self, supply, parent = None):
+    def __init__(self, supply, setter, parent = None):
         super().__init__(parent)
         self.supply = supply
+        self.setter = setter
 
     @Slot()
     def emit_new_values(self):
+        """
+        Sets the measurement_changed signal and emits the signal to update the UI
+        """
         self.supply.measureValues()
         voltage = self.supply.measuredpoints.voltage
         current = self.supply.measuredpoints.current
         power = self.supply.measuredpoints.power
         self.measurement_changed.emit(voltage, current, power)
-
-measure_signal = psu_measure_signal(supply)
+    def determine_set_output(self):
+        """
+        Takes the measured values stored within the supply object and uses them to determine the next value to set for the power supply 
+        before sending the results to the supply.
+        """
+        self.supply.setValues(self.setter.u_for_i_incremental(self.supply.measuredpoints.current))
+        return
+    def measure_emit_set(self):
+        """
+        Measures the values from the supply, emits the signal to the UI to update the Display 
+        """
+        self.emit_new_values()
+        self.determine_set_output()
+        
+# functions from the before created clases to be added to the scheduler
+measure_signal = psu_measure_signal(supply, set_supply)
--- main.py
+++ main.py
@@ -1,49 +1,38 @@
-import curveutils, gui_signals, qt_scheduler
+import gui_signals, qt_scheduler
 import signal
 from PySide6.QtWidgets import QApplication
- # connect to power supply with this IP Address
-
-#create two vectors and populate them with the values from a one diode model
-U_1, I_1 = curveutils.solarIV(4, 50, 8.75e-3, 4.0, 25.7e-3, 3e-3, 1000, 10000)
-#prepare data before running the controll algorithm, removes too low 
-U_1, I_1 = curveutils.min_remover(U_1, I_1, 5)
-U_1, I_1 = curveutils.stepsize_reducer(list(U_1), list(I_1), 0.025, 'right')
-
-#eddited the value of the initial voltage to 5, by suggestion by Rüdiger Mann 27.04.26
-gui_signals.supply.setValues(5, I_1.max()*1.1, 3000.0)
-
-set_supply = curveutils.setter(U_1, I_1)
-
+"""
 def set(supply, setter):
     supply.setValues(setter.u_for_i_incremental(supply.measuredpoints.current))
     return
-    
+""" 
 def print_measured_points(supply):
     print(supply.measuredpoints)
     return
     
 psu_com = qt_scheduler.semaphore(semaphore_name="psu_com")
 
-set_supply = curveutils.setter(U_1, I_1)
 sched = qt_scheduler.Scheduler(tick_ms=1)
 sched.add_periodic(
     "measure",
     period_s=0.01,
-    func=gui_signals.measure_signal.emit_new_values,
+    func=gui_signals.measure_signal.measure_emit_set,
     args=(),          # only supply is static
     kwargs={},
     start_immediately=True,
     semaphores=[psu_com]
 )
+"""
 sched.add_periodic(
     "set",
     period_s=0.01,
     func=set,
-    args=(gui_signals.supply,set_supply,),          # only supply is static
+    args=(gui_signals.supply,gui_signals.set_supply,),          # only supply is static
     kwargs={},
     start_immediately=True,
     semaphores=[psu_com]
 )
+"""
 sched.add_periodic(
     "print",
     period_s=1,
@@ -55,8 +44,6 @@ sched.add_periodic(
 app = QApplication([])
 gui_window = gui_signals.MainDialog()
 gui_window.show()
-
-gui_signals.supply.setValues(5, I_1.max()*1.1, 3000.0)
 
 sched.start_all()
 
