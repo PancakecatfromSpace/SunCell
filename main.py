b8bd1a56b806ab7d98706f39fa984c222cd20f3a
Samuel H <s230067@th-ab.de>
2026-07-08 18:34:50 +0200

    added code to try out the "run only once" featire of the scheduler

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
+"""
+To whever is cursed to edit this after I'm gone, Professor Teigelkötter told me that this programm is irrelevant and that I should do
+it past work hours. I'm not getting payed, I have to spend time outside of my studies to get the funds for my studies. I'm tired of 
+this. These guys wanted me gone from day one. Think about your situation. Is your time worthless? Should your efforts always
+be in vain? Then stay here and keep working for this guy. I won't.
+"""
+
 import gui_signals, qt_scheduler
 import signal
 from PySide6.QtWidgets import QApplication
-"""
-def set(supply, setter):
-    supply.setValues(setter.u_for_i_incremental(supply.measuredpoints.current))
-    return
-""" 
+
 def print_measured_points(supply):
     print(supply.measuredpoints)
+    return
+def print_once():
+    print("This only ran once.")
     return
     
 psu_com = qt_scheduler.semaphore(semaphore_name="psu_com")
 
 sched = qt_scheduler.Scheduler(tick_ms=1)
 sched.add_periodic(
-    "measure",
+    "measure_set",
     period_s=0.01,
     func=gui_signals.measure_signal.measure_emit_set,
     args=(),          # only supply is static
@@ -22,17 +28,7 @@ sched.add_periodic(
     start_immediately=True,
     semaphores=[psu_com]
 )
-"""
-sched.add_periodic(
-    "set",
-    period_s=0.01,
-    func=set,
-    args=(gui_signals.supply,gui_signals.set_supply,),          # only supply is static
-    kwargs={},
-    start_immediately=True,
-    semaphores=[psu_com]
-)
-"""
+
 sched.add_periodic(
     "print",
     period_s=1,
@@ -41,6 +37,17 @@ sched.add_periodic(
     kwargs={},
     start_immediately=True
 )
+sched.add_periodic(
+    "print_once",
+    period_s=0,
+    func=print_once,
+    args=(),          # only supply is static
+    kwargs={},
+    start_immediately=True
+)
+
+
+
 app = QApplication([])
 gui_window = gui_signals.MainDialog()
 gui_window.show()
